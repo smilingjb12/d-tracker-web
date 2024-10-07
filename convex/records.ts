0f6e2f4f76731dd17fd635c442e5169bfce4d253
Kiril Pashkov <k.pashkov@itransition.com>
2024-10-07 21:01:45 +0200

    Restricting record modification to admins only

--- convex/records.ts
+++ convex/records.ts
@@ -1,5 +1,6 @@
 import { paginationOptsValidator } from "convex/server";
 import { v } from "convex/values";
+import { api } from "./_generated/api";
 import { internalMutation, query } from "./_generated/server";
 import { getRecordsPageHandler } from "./handlers/records";
 import { requireAuthentication } from "./lib/helpers";
@@ -15,6 +16,10 @@ export const getRecordsPage = query({
 export const addRecord = internalMutation({
   args: { latitude: v.number(), longitude: v.number(), power: v.number() },
   handler: async (ctx, args) => {
+    const user = await ctx.runQuery(api.users.getCurrentUser, {});
+    if (!user?.isAdmin) {
+      throw new Error("Only admins can add records");
+    }
     return await ctx.db.insert("records", args);
   },
 });
