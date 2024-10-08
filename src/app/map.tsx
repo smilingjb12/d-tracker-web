9b211b84908870af29dfd8fe30ae6179cc429491
Kiril Pashkov <k.pashkov@itransition.com>
2024-10-08 19:54:15 +0200

    Change map tiles

--- src/app/map.tsx
+++ src/app/map.tsx
@@ -40,7 +40,7 @@ export default function LocationMap() {
         style={{ height: "400px", width: "100%" }}
       >
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <Marker position={point} icon={customIcon} />
