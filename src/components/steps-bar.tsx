87805292a584b8a193418143607af3fbbd5d2aa5
smilingjb12 <smilingjb12@gmail.com>
2025-02-06 15:56:25 +0100

    Display steps in logs table

--- src/components/steps-bar.tsx
+++ src/components/steps-bar.tsx
@@ -1,8 +1,11 @@
+import { cn } from "@/lib/utils";
+
 type StepsBarProps = {
   steps: number;
+  showBar?: boolean;
 };
 
-export function StepsBar({ steps }: StepsBarProps) {
+export function StepsBar({ steps, showBar = true }: StepsBarProps) {
   const MAX_STEPS = 6000;
   const percentage = Math.min(Math.max((steps / MAX_STEPS) * 100, 0), 100);
   const color =
@@ -12,17 +15,24 @@ export function StepsBar({ steps }: StepsBarProps) {
   const textColor = steps > MAX_STEPS ? "#D946EF" : color;
 
   return (
-    <div className="flex flex-col items-center w-full gap-1">
+    <div
+      className={cn(
+        "flex flex-col w-full gap-1",
+        showBar ? "items-center" : "items-start"
+      )}
+    >
       <span style={{ color: textColor }}>{steps}</span>
-      <div className="w-32 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-        <div
-          className="h-2.5 rounded-full"
-          style={{
-            width: steps > MAX_STEPS ? "100%" : `${percentage}%`,
-            background: color,
-          }}
-        ></div>
-      </div>
+      {showBar && (
+        <div className="w-32 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+          <div
+            className="h-2.5 rounded-full"
+            style={{
+              width: steps > MAX_STEPS ? "100%" : `${percentage}%`,
+              background: color,
+            }}
+          ></div>
+        </div>
+      )}
     </div>
   );
 }
