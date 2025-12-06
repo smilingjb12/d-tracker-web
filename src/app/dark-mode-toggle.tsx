b4b01f75335b70c72d9eb3e025c41d6c7bea98ac
smilingjb12 <smilingjb12@gmail.com>
2025-12-06 17:59:50 +0100

    ci fix

--- src/app/dark-mode-toggle.tsx
+++ src/app/dark-mode-toggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -15,8 +15,8 @@ export function DarkModeToggle() {
 
   return (
     <Button variant="outline" size="icon" onClick={toggleTheme}>
-      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
-      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
